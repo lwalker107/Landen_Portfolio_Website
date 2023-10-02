cc3fc253cd90d68bed562532c8216e316945d6fc
lwalker <landen@example.com>
2023-10-01 22:32:17 -0500

    added a test folder + setup testing configurations in vite.config file + package.json

--- Develop/Portfolio Website/vite.config.js
+++ Develop/Portfolio Website/vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+  },
   server: {
     port: 3000,
     open: true
   }
-})
+});
